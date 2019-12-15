476e8f117f1c4f2d8c0982db07e03814dc716da0
Vasco Candeias <vascocandeias@users.noreply.github.com>
2019-12-15 17:21:32 +0000

    warning fix

--- backend/webpages/webpages.py
+++ backend/webpages/webpages.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-from datetime import *
+from datetime import datetime
 import logging
 import json
 import requests
